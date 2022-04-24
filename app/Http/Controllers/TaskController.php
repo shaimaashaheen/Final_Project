6846b4bcd41ffb70a559ff62a52c0517a7e76870
shaimaashaheen <shaimaa.shaheen1999@gmail.com>
2022-04-24 23:43:01 +0300

    Eloquent commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,19 +10,21 @@ use Illuminate\Support\Facades\Redirect;
 class TaskController extends Controller
 {
   public function index(){
-      $tasks = DB::table('tasks')
-      -> orderBy('name','asc')
-      ->get();
-      //$tasks = Task::all();
+      //$tasks = DB::table('tasks')
+      //-> orderBy('name','asc')
+      //->get();
+
+      $tasks = Task::all();
       return view('tasks',compact('tasks'));
   }
 
   public function store(Request $request){
-      //DB::table('tasks')->insert([
-        //  'name' => $request->name,
-          //'created_at' => now(),
+     // DB::table('tasks')->insert([
+       //   'name' => $request->name,
+         // 'created_at' => now(),
           //'updated_at' => now()
-     // ]);
+      //]);
+
      $task = new Task();
      $task ->name = $request -> name;
      $task -> save();
@@ -31,21 +33,27 @@ class TaskController extends Controller
 
   public function delete($id){
      // DB::table('tasks')->where('id','=', $id)->delete();
-     Task::find($id) -> delete();
+
+      Task::find($id) -> delete();
       return redirect()->back();
   }
 
   public function update($id){
-      $tasks = DB::table('tasks') -> where('id',$id) -> first();
+      //$tasks = DB::table('tasks') -> where('id',$id) -> first();
+      $tasks = Task::find($id);
      return view('update' , compact('tasks'));
  }
 
  public function Edit(Request $request ,$id){
-     $data = array();
-     $data['name'] = $request -> name;
+     //$data = array();
+     //$data['name'] = $request -> name;
     // $data['id'] = $request -> id;
-     DB::table('tasks') -> where('id', $id) -> update($data);
-     $tasks = Task::all();
-     return view('tasks',compact('tasks'));
+     //DB::table('tasks') -> where('id', $id) -> update($data);
+     //$tasks = Task::all();
+
+     $task = Task::find($id);
+     $task -> name = $request -> name;
+     $task -> save();
+     return 'Task Name Updated';
 }
 }
